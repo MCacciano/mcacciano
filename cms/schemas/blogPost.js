2578d7d222b23dc9a27a2126c9713913e757b696
mcacciano <michael.cacciano@gmail.com>
2020-03-04 06:25:23 -0500

    able to run sanity studio, able to see sanity content in graphql graph after build (content making it to gatbsy)

---
+++ cms/schemas/blogPost.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'blogPost',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'sponsor',
+      title: 'Sponsor',
+      type: 'sponsor'
+    }
+  ]
+};
