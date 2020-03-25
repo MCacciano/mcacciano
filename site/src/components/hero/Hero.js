6133f1464259d1fc683623a674bff3c1f4bdd397
Michael <michael.cacciano@gmail.com>
2020-03-25 07:13:07 -0400

    remove old styles
    remove commented out code from older styles

--- site/src/components/hero/Hero.js
+++ site/src/components/hero/Hero.js
@@ -9,9 +9,6 @@ import {
   HeroCopyWrapper,
   HeroCopy,
   Span,
-  SocialList,
-  SocialListItem,
-  SocialListIcon,
   H1,
   H3,
   H2,
@@ -38,21 +35,17 @@ const Hero = () => {
     { icon: 'linkedin', href: 'https://www.linkedin.com/in/michaelcacciano/' },
   ]
 
-  const trail = useTrail(SOCIAL_LINKS.length, {
-    transform: 'scale(1)',
-    opacity: 1,
-    trail: 500,
-    from: { transform: 'scale(0.1)', opacity: '0.1' },
-  })
+  // const trail = useTrail(SOCIAL_LINKS.length, {
+  //   transform: 'scale(1)',
+  //   opacity: 1,
+  //   trail: 500,
+  //   from: { transform: 'scale(0.1)', opacity: '0.1' },
+  // })
 
   return (
     <HeroWrapper imageData={imageData}>
       <div class="overlay overlay-black"></div>
       <div class="overlay overlay-blue"></div>
-      {/* <div id="oval-1" class="hero-oval"></div> */}
-      {/* <div id="oval-1" class="hero-oval"></div>
-      <div id="oval-2" class="hero-oval"></div>
-      <div id="oval-3" class="hero-oval"></div> */}
       <HeroCopyWrapper>
         <HeroCopy>
           <H1>
@@ -99,54 +92,6 @@ const Hero = () => {
           </SocialList> */}
         </HeroCopy>
       </HeroCopyWrapper>
-      {/* q<HeroCopyWrapper>
-        <HeroCopy>
-          <SocialList>
-            {trail.map(({ ...styles }, i) => {
-              const { href, icon } = SOCIAL_LINKS[i]
-
-              return (
-                <SocialListItem key={i} style={{ ...styles }}>
-                  <a
-                    href={href}
-                    className={icon}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <SocialListIcon className={`fab fa-${icon} fa-2x`} />
-                  </a>
-                </SocialListItem>
-              )
-            })}
-          </SocialList>
-          <div>
-            <h1>
-              <Span hWidth={97} left="4px">
-                Michael Cacciano{' '}
-              </Span>
-            </h1>
-            <h2>
-              <Span>Front </Span>
-              <Span>End </Span>
-              <Span>Developer </Span>
-            </h2>
-            <h3>
-              <Span fontWeight={300} hWidth={89} left="3px">
-                Passion{' '}
-              </Span>
-              for creating{' '}
-              <Span fontWeight={300} hWidth={90} left="2px">
-                simplistic{' '}
-              </Span>
-              and{' '}
-              <Span fontWeight={300} hWidth={92} left="2px">
-                engaging{' '}
-              </Span>
-              experiences on the web
-            </h3>
-          </div>
-        </HeroCopy>
-      </HeroCopyWrapper> */}
     </HeroWrapper>
   )
 }
--- site/src/components/hero/Hero.styles.js
+++ site/src/components/hero/Hero.styles.js
@@ -1,5 +1,5 @@
 import styled, { keyframes } from 'styled-components'
-import { animated } from 'react-spring'
+// import { animated } from 'react-spring'
 
 import BackgroundImg from '../background-img/BackgroundImg'
 
@@ -21,19 +21,14 @@ export const HeroWrapper = styled(BackgroundImg)`
   position: relative;
   background-position: bottom right;
 
-  .hero-oval {
+  /* .hero-oval {
     position: absolute;
     left: 0;
-    /* clip-path: ellipse(); */
+    clip-path: ellipse();
     width: 100%;
     z-index: -101;
 
     &#oval-1 {
-      bottom: -45px;
-      height: 100px;
-      background: rgba(50, 130, 184, 0.3);
-    }
-    /* &#oval-1 {
       bottom: -45px;
       height: 100px;
       background: rgba(50, 130, 184, 0.3);
@@ -49,8 +44,8 @@ export const HeroWrapper = styled(BackgroundImg)`
       height: 100px;
       background: rgba(50, 130, 184, 0.6);
       transform: rotate(-2deg);
-    } */
-  }
+    }
+  } */
 
   .overlay {
     display: block;
@@ -166,137 +161,3 @@ export const Span = styled.span`
     animation: ${growRight} ease-in 1s forwards;
   }
 `
-
-// export const HeroCopyWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   background: #f7f7f7;
-
-//   flex: 0 1 60%;
-//   z-index: 1;
-
-//   @media screen and (max-width: 900px) {
-//     background: transparent;
-//     flex: 1;
-//   }
-
-//   &:after {
-//     content: '';
-//     display: block;
-//     width: 40%;
-//     height: 100%;
-//     position: absolute;
-//     background: rgba(0, 0, 0, 0.3);
-//     top: 0;
-//     right: 0;
-
-//     @media screen and (max-width: 900px) {
-//       width: 100%;
-//     }
-//   }
-// `
-
-/**
- * @media screen and (max-width: 900px) {
-    background: #f7f7f7;
-    width: 100%;
-    justify-content: center;
-  }
-
-  @media screen and (max-width: 630px) {
-    flex-direction: column-reverse;
-  }
-
-  div {
-    max-width: 656px;
-
-    @media screen and (max-width: 1260px) {
-      max-width: 432px;
-    }
-  }
-
-  h1,
-  h2,
-  h3 {
-    display: inline-block;
-  }
-
-  h1 {
-    font-size: 4.3rem;
-    font-weight: 900;
-    font-family: 'Rubik', Arial, Helvetica, sans-serif;
-
-    @media screen and (max-width: 1260px) {
-      font-size: 2.8rem;
-    }
-
-    @media screen and (max-width: 481px) {
-      font-size: 2.3rem;
-    }
-
-    @media screen and (max-width: 385px) {
-      font-size: 2.1rem;
-    }
-    @media screen and (max-width: 360px) {
-      font-size: 1.7rem;
-    }
-  }
-  h2 {
-    font-size: 3.5rem;
-    font-weight: 400;
-    padding-top: 0.2rem;
-    font-family: 'Rubik', Arial, Helvetica, sans-serif;
-
-    @media screen and (max-width: 1260px) {
-      font-size: 2.22rem;
-    }
-
-    @media screen and (max-width: 481px) {
-      font-size: 2.15rem;
-    }
-
-    @media screen and (max-width: 385px) {
-      padding-top: 0.5rem;
-      font-size: 1.4rem;
-    }
-  }
-  h3 {
-    font-size: 2.5rem;
-    font-weight: 100;
-    margin-top: 1.25rem;
-
-    @media screen and (max-width: 1260px) {
-      font-size: 1.75rem;
-    }
-
-    @media screen and (max-width: 481px) {
-      font-size: 1.55rem;
-    }
-
-    @media screen and (max-width: 385px) {
-      font-size: 1.25rem;
-    }
-  }
- */
-// export const SocialList = styled.ul`
-//   display: flex;
-//   flex-flow: column nowrap;
-//   justify-content: flex-start;
-//   margin-right: 1rem;
-
-//   @media screen and (max-width: 630px) {
-//     flex-direction: row;
-//   }
-// `
-
-// export const SocialListItem = styled(animated.li)`
-//   margin-top: 0.9rem;
-
-//   @media screen and (max-width: 630px) {
-//     &:not(:first-child) {
-//       margin-left: 1.25rem;
-//     }
-//   }
-// `
-
-// export const SocialListIcon = styled(animated.i)``
