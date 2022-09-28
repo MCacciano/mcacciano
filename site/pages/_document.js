160de564f25902dc3ad8dfcdc5d9a25dc8d744a4
Michael <michael.cacciano@gmail.com>
2022-09-28 08:32:09 -0400

    update yarn to v2

--- site/pages/_document.js
+++ site/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Rubik:wght@300;400;500;600;700;800;900&display=swap'
           rel='stylesheet'
